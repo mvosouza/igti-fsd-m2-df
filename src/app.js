8f1ba6c074948161c814fd4a15dc3eb500dea72e
Marcos Souza <marcos.oliversouza@gmail.com>
2020-06-01 15:22:18 -0300

    add basic express server configuration

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+import express from 'express';
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get('/grades', (req, res) => {
+  res.send('Recieved!');
+});
+
+app.listen(port, () => {
+  try {
+    console.log(`Server is running on port ${port}`);
+  } catch (err) {
+    console.error(err);
+  }
+});
