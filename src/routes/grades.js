fffbd8b7a822aaa4b9e97b4bd85321aac421b275
Marcos Souza <marcos.oliversouza@gmail.com>
2020-06-01 16:00:59 -0300

    Added grade update route

--- src/routes/grades.js
+++ src/routes/grades.js
@@ -4,12 +4,22 @@ import { promises as fs } from 'fs';
 const router = express.Router();
 const filePath = './src/data/grades.json';
 
+//Functions
+const readFileAsJson = async (file) => {
+  const rawData = await fs.readFile(file, 'utf8');
+  return JSON.parse(rawData);
+};
+
+/************
+ *  Routes  *
+ ************/
+
+//Item 01
 router.post('/', async (req, res) => {
   try {
     const params = req.body;
 
-    const rawData = await fs.readFile(filePath, 'utf8');
-    const gradesFile = JSON.parse(rawData);
+    const gradesFile = await readFileAsJson(filePath);
     const newGrade = {
       id: gradesFile.nextId++,
       ...params,
@@ -19,6 +29,29 @@ router.post('/', async (req, res) => {
     await fs.writeFile(filePath, JSON.stringify(gradesFile));
 
     res.status(201).send(newGrade);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+//Item 02
+router.put('/', async (req, res) => {
+  try {
+    const params = req.body;
+
+    const gradesFile = await readFileAsJson(filePath);
+    const index = gradesFile.grades.findIndex(
+      (grade) => grade.id === parseInt(params.id, 10)
+    );
+
+    if (index == -1) {
+      res.status(404).send();
+      return;
+    }
+    gradesFile.grades[index] = { ...params, timestamp: new Date() };
+    await fs.writeFile(filePath, JSON.stringify(gradesFile));
+    res.send();
   } catch (err) {
     console.error(err);
     res.status(500).send({ message: err.message });
