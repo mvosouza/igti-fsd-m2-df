04ac5f64cec21d40688d6577266f654f7a023b06
Marcos Souza <marcos.oliversouza@gmail.com>
2020-06-01 20:32:19 -0300

    Added grade delete route

--- src/routes/grades.js
+++ src/routes/grades.js
@@ -46,12 +46,38 @@ router.put('/', async (req, res) => {
     );
 
     if (index == -1) {
-      res.status(404).send();
+      res.status(404).send('Grade not found!');
       return;
     }
     gradesFile.grades[index] = { ...params, timestamp: new Date() };
     await fs.writeFile(filePath, JSON.stringify(gradesFile));
     res.send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+//Item 03
+router.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const gradesFile = await readFileAsJson(filePath);
+    const grade = gradesFile.grades.find(
+      (grade) => grade.id === parseInt(id, 10)
+    );
+
+    if (!grade) {
+      res.status(404).send('Grade not found!');
+      return;
+    }
+
+    gradesFile.grades = gradesFile.grades.filter(
+      (grade) => grade.id !== parseInt(id)
+    );
+    await fs.writeFile(filePath, JSON.stringify(gradesFile));
+    res.send(grade);
   } catch (err) {
     console.error(err);
     res.status(500).send({ message: err.message });
